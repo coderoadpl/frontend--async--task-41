fe7bd13aaae12a3ebcafe1a025dc331bd96a95c8
Mateusz Choma <kontakt@coderoad.pl>
2021-03-23 21:48:46 +0100

    Catching errors

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 const fetchUsers = async () => {
-    const response = await fetch('https://randomuser.me/api?results=10')
-
-    const data = await response.json()
-
-    return data
+    try {
+        const response = await fetch('https://randomuser.MEEE/api?results=10')
+        const data = await response.json()
+        return data
+    } catch (error) {
+        console.log(error)
+        throw new Error('Our custom error')
+    }
 }
 
-console.log(fetchUsers())
+(async () => {
+
+    try {
+        console.log(
+            await fetchUsers()
+        )
+    } catch (error) {
+        console.log(error)
+    }
+
+})()
