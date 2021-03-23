e2bc06c17bb44d3b92e68482628d111ff606dd71
Mateusz Choma <kontakt@coderoad.pl>
2021-03-23 21:29:39 +0100

    async functions vs promises ones

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-const returnX = async function(x){
+// this commented line will not work because top level async/await works only in modules
+// await new Promise((resolve) => setTimeout(() => resolve(5), 3000))
+
+// regular await works only inside async functions as below
+
+const returnX = async function (x) {
     return x
 }
 
@@ -9,3 +14,37 @@ const returnXArrow = async (x) => {
 }
 
 console.log(returnXArrow(5))
+
+const returnSumOfPromisesValues = (x) => {
+    const promise5 = () => new Promise((resolve) => setTimeout(() => resolve(5), 3000))
+    const promise10 = () => new Promise((resolve) => setTimeout(() => resolve(10), 3000))
+
+    const allPromise = Promise.all([
+        promise5(),
+        promise10(),
+    ])
+
+    const sumPromise = allPromise.then((results) => results.reduce((r, el) => r + el, 0))
+
+    return sumPromise
+}
+
+console.log(
+    returnSumOfPromisesValues()
+        .then((sum) => console.log('returnSumOfPromisesValues', sum))
+)
+
+const returnSumOfPromisesValuesAsync = async (x) => {
+    const promise5 = () => new Promise((resolve) => setTimeout(() => resolve(5), 3000))
+    const promise10 = () => new Promise((resolve) => setTimeout(() => resolve(10), 3000))
+
+    const number5 = await promise5()
+    const number10 = await promise10()
+
+    return number5 + number10
+}
+
+console.log(
+    returnSumOfPromisesValuesAsync()
+        .then((sum) => console.log('returnSumOfPromisesValuesAsync', sum))
+)
