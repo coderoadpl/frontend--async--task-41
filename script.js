29ec47abc0c98d558e3a7a64c247f8ae0aa6069c
Mateusz Choma <kontakt@coderoad.pl>
2021-03-23 21:36:06 +0100

    fetching data using async await

--- script.js
+++ script.js
@@ -1,50 +1,9 @@
-// this commented line will not work because top level async/await works only in modules
-// await new Promise((resolve) => setTimeout(() => resolve(5), 3000))
+const fetchUsers = async () => {
+    const response = await fetch('https://randomuser.me/api?results=10')
 
-// regular await works only inside async functions as below
+    const data = await response.json()
 
-const returnX = async function (x) {
-    return x
+    return data
 }
 
-console.log(returnX(5))
-
-const returnXArrow = async (x) => {
-    return x
-}
-
-console.log(returnXArrow(5))
-
-const returnSumOfPromisesValues = (x) => {
-    const promise5 = () => new Promise((resolve) => setTimeout(() => resolve(5), 3000))
-    const promise10 = () => new Promise((resolve) => setTimeout(() => resolve(10), 3000))
-
-    const allPromise = Promise.all([
-        promise5(),
-        promise10(),
-    ])
-
-    const sumPromise = allPromise.then((results) => results.reduce((r, el) => r + el, 0))
-
-    return sumPromise
-}
-
-console.log(
-    returnSumOfPromisesValues()
-        .then((sum) => console.log('returnSumOfPromisesValues', sum))
-)
-
-const returnSumOfPromisesValuesAsync = async (x) => {
-    const promise5 = () => new Promise((resolve) => setTimeout(() => resolve(5), 3000))
-    const promise10 = () => new Promise((resolve) => setTimeout(() => resolve(10), 3000))
-
-    const number5 = await promise5()
-    const number10 = await promise10()
-
-    return number5 + number10
-}
-
-console.log(
-    returnSumOfPromisesValuesAsync()
-        .then((sum) => console.log('returnSumOfPromisesValuesAsync', sum))
-)
+console.log(fetchUsers())
